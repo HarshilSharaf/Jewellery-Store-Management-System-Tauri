d3c8ea5037ac3e1a4076b82de2e512f9197f06fb
Harshil Sharaf <harshil.sharaf1234@gmail.com>
2023-10-04 00:36:41 +0530

    Add popup to show error message if db connection fails

--- Backend/Shared/database.service.ts
+++ Backend/Shared/database.service.ts
@@ -2,6 +2,8 @@ import { SettingsModel } from 'client/app/modules/settings/models/settings-model
 import Database from "tauri-plugin-sql-api";
 import { StoreService } from './store.service';
 import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,7 @@ export class DatabaseService {
 
   public dbConnection:any;
   private dbConnectionInfo!:SettingsModel
-  constructor(private storeService:StoreService) { }
+  constructor(private storeService:StoreService, private router: Router) { }
 
   initializeDbConnection():Promise<any> {
     return new Promise(async (resolve,reject) => {
@@ -28,12 +30,45 @@ export class DatabaseService {
         try {
           this.dbConnection = await Database.load(dbURL);
         } catch (error) {
-          let timerInterval: any;
-          throw error
+          this.showErrorAndRedirectToSettingsPage(error);
         }
       
       
       resolve(this.dbConnection);
     })
+  }
+
+  showErrorAndRedirectToSettingsPage(error: any) {
+    let timerInterval: any;
+    Swal.fire({
+      title: 'Could Not Connect To Database!',
+      html: `<span class="text-danger"> Error: ${error} 
+        <p class="text-warning my-2"> Redirecting to Settings Page...</p>
+        </span>`,
+      timer: 4000,
+      timerProgressBar: true,
+      allowEscapeKey : false,
+      allowOutsideClick: false,
+      didOpen: () => {
+        Swal.showLoading();
+        const b = Swal.getHtmlContainer()?.querySelector(
+          'b'
+        ) as HTMLElement;
+        timerInterval = setInterval(() => {
+          b.textContent = Swal.getTimerLeft()?.toString() ?? '000000';
+        }, 100);
+      },
+      willClose: () => {
+        clearInterval(timerInterval);
+      },
+    }).then((result) => {
+      if (result.dismiss === Swal.DismissReason.timer) {
+        this.router.navigate(['settings'], {
+          state: {
+            error: error,
+          },
+        });
+      }
+    });
   }
 }
